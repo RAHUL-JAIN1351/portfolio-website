10c46be38df742159225f807b1068bca757af30e
RAHUL-JAIN1351 <65061254+RAHUL-JAIN1351@users.noreply.github.com>
2020-06-10 22:01:19 -0700

    Create in.js

---
+++ js/in.js
@@ -0,0 +1,24 @@
+       var i=0;
+       function f(){
+          document.getElementById('one').classList.toggle('do');
+       }
+
+       function fun() {
+          if(i == 0){
+            document.getElementById('bar1').style.transform = 'rotate(-45deg) translate(-10px,10px)';
+            document.getElementById('bar3').style.transform = 'rotate(45deg) translate(-7px,-5px)';
+            document.getElementById('bar2').style.opacity = '0';
+            document.getElementById('no').style.display = 'block';
+
+            i=1;
+          }
+          else{
+            document.getElementById('bar1').style.transform = 'rotate(0deg) ';
+            document.getElementById('bar3').style.transform = 'rotate(0deg) ';
+            document.getElementById('bar2').style.opacity = '1';
+            document.getElementById('no').style.display = 'none';
+
+            i=0;
+          }
+
+       }
