6dd708b3e546430c1549073f433d3cde9f33c922
RAHUL-JAIN1351 <65061254+RAHUL-JAIN1351@users.noreply.github.com>
2020-06-12 20:34:51 +0530

    Update in.js

--- js/in.js
+++ js/in.js
@@ -3,7 +3,7 @@
           document.getElementById('one').classList.toggle('do');
      
           if(cnt == 1){
-               document.getElementById('one').style.animation = "bulge 0.5s ease 1s infinite alternate";
+               document.getElementById('one').style.animation = "bulge 0.5s ease 0.5s infinite alternate";
                cnt=0;
           }
           else if(cnt == 0){
